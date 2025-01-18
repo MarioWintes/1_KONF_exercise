04167f503687f9ac20a5b7ad1ee8d403f083f81f
MarioWintes <52406123@edu.campus02.at>
2025-01-18 10:26:28 +0100

    Konstruktor test

--- src/test/java/org/campus02/winterauer/BMITest.java
+++ src/test/java/org/campus02/winterauer/BMITest.java
@@ -12,7 +12,19 @@ public class BMITest {
     }
 
     @Test
-    void testBMI() {
-        Assertions.assertTrue(true);
+    void testConstructor() {
+        String expectedFirstName = "Hermann";
+        String expectedLastName = "Maier";
+        int expectedHeight = 185;
+        double expectedWeight = 95;
+        char expectedGender = 'M';
+
+        BMICalculator bmiCalculator = new BMICalculator(expectedFirstName, expectedLastName, expectedHeight, expectedWeight, expectedGender);
+
+        Assertions.assertEquals(expectedFirstName, bmiCalculator.getFirstName());
+        Assertions.assertEquals(expectedLastName, bmiCalculator.getLastName());
+        Assertions.assertEquals(expectedHeight, bmiCalculator.getBodyHeight());
+        Assertions.assertEquals(expectedWeight, bmiCalculator.getBodyWeight());
+        Assertions.assertEquals(expectedGender, bmiCalculator.getGender());
     }
 }
