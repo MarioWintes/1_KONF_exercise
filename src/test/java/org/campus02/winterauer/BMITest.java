7fbe40769571d0965856c42c5163a4cc93144864
MarioWintes <52406123@edu.campus02.at>
2025-01-18 10:18:11 +0100

    1st test

--- src/test/java/org/campus02/winterauer/BMITest.java
+++ src/test/java/org/campus02/winterauer/BMITest.java
@@ -1,11 +1,18 @@
 package org.campus02.winterauer;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class BMITest {
 
     @BeforeEach
     public void setUp() {
-        
+
+    }
+
+    @Test
+    void testBMI() {
+        Assertions.assertTrue(true);
     }
 }
